c3161cbcaa8b09dd0bdc7f48a7bb0b8df791d527
Leanstix <leanstixx@gmail.com>
2025-03-27 21:40:28 +0100

    setting the models

--- Dahliacore/urls.py
+++ Dahliacore/urls.py
@@ -8,6 +8,23 @@ from django.conf.urls.static import static
 from django.http import JsonResponse
 from django.urls import include
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dahliacore API",
+        default_version="v1",
+        description="API documentation for Dahliacore",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=opensapi.Contact(email="leanstixX@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schools/', include('SchoolsAuth.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', lambda request: JsonResponse({'message': 'Welcome to the Dahliacore API!'})),
 ]
