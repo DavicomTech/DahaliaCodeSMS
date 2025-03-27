14d63cb0c2f526f06dd89b55e15d6ee161a6681f
Leanstix <leanstixx@gmail.com>
2025-03-27 21:20:15 +0100

    setting the models

--- Dahliacore/settings.py
+++ Dahliacore/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'django.contrib.flatpages',
+    'django.contrib.flatpages',
+    'SchoolsAuth',
 ]
 
 MIDDLEWARE = [
