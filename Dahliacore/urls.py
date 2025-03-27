c968de47e1ba4885b8171bd71862adc2f53fdb22
Leanstix <leanstixx@gmail.com>
2025-03-27 21:44:54 +0100

    setting the models

--- Dahliacore/urls.py
+++ Dahliacore/urls.py
@@ -12,7 +12,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Dahliacore API",
         default_version="v1",
-        description="API documentation for Dahliacore",
+        description="API documentation for Dahliacode",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=opensapi.Contact(email="leanstixX@gmail.com"),
         license=openapi.License(name="BSD License"),
@@ -26,7 +26,7 @@ urlpatterns = [
     path('api/schools/', include('SchoolsAuth.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', lambda request: JsonResponse({'message': 'Welcome to the Dahliacore API!'})),
+    path('', lambda request: JsonResponse({'message': 'Welcome to the Dahliacode API!'})),
 ]
 
 if settings.DEBUG:  # Serve media files only in development
